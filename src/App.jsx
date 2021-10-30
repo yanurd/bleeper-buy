bdd42e54e77d4ac974db8c062ef4af34a3cc3ba4
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-30 11:23:00 -0400

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import "tailwindcss/tailwind.css"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p className="text-blue-600">Hello Vite + React!</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
