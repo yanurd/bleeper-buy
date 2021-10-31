acec84101b2250fb89448c15ad23ac36b4c08b0b
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-30 20:04:58 -0400

    useproducts hook

--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -1,1 +1,34 @@
-import {useState,useEffect } from 'react'
+import {useState, useEffect } from 'react'
+
+import {API_URL} from '../utils/index'
+
+export function useProducts() {
+  const [loading,setLoading] = useState(false)
+  const [error, setError] = useState(false)
+  const [products, setProducts] = useState([])
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      setLoading(true)
+    fetch(API_URL)
+    .then(data => data.json())
+    .then(response => setProducts(response))
+    .catch(()=> {
+      setLoading(false)
+      setError(true)
+    })
+    .finally(()=> setLoading(false))
+    
+    return function cleanup(){
+      unsuscribe()
+    }
+    },3000)
+    
+  },[])
+
+  return {
+    loading,
+    error,
+    products
+  }
+}
