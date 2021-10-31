4c24332564c476b693f842cf44ac7737c070ad9c
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-10-30 20:05:30 -0400

    Add context provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
 import "tailwindcss/tailwind.css"
 
+import {BleeperProvider} from './context/index.jsx'
+
+import AppUi from './containers/AppUi'
 function App() {
   return (
-    <></>
+    <BleeperProvider>
+      <AppUi />
+    </BleeperProvider>
   )
 }
 
