734231f81fefaea4f9b8f1b12ad0c14919c9ff1a
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-12 11:47:23 -0400

    import context

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,12 +1,37 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
 import { useProducts } from '../hooks/useProducts'
 
 const BleeperContext = createContext()
 
 const BleeperProvider = (props) => {
-	const {loading,error,products} = useProducts()
+	const {loading,error,products} = useProducts();
+	const [search, setSearch] = useState("");
+
+	let searchedProducts = [];
+
+	if (!search.length >= 1) {
+		searchedProducts = products
+	}
+	else {
+		searchedProducts =  products?.filter(product => {
+			const itemName = product.title.toLowerCase()
+			const searchedItem = search.toLowerCase()
+			return itemName.includes(searchedItem)
+		})
+	}
+
+	const onSearchChange = (event) => {
+		setSearch(event.target.value)
+	}
 	return (
-		<BleeperContext.Provider value={{error,loading,products}}>
+		<BleeperContext.Provider 
+			value={{
+				error,
+				loading,
+				searchedProducts,
+				search,
+				onSearchChange,
+			}}>
 		{props.children}
 		</BleeperContext.Provider>
 	)
