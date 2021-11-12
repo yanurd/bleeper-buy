366a737bf6b78ec43342dd62a4bbb5630c45bb96
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-12 11:46:38 -0400

    remove h2 and add sidebar

--- src/containers/AppUi.jsx
+++ src/containers/AppUi.jsx
@@ -1,7 +1,22 @@
+import {useContext} from 'react'
+import {BleeperContext} from '../context'
+import {ProductItem} from '../components/ProductItem'
+
+import {FilterMethods} from '../containers/FilterMethods'
+import SideBar from './SideBar'
 const AppUi = () =>{
+  const {loading, error} = useContext(BleeperContext)
   return (
     <>
-      <h2 className="">Hola</h2>
+      <SideBar/>
+      <FilterMethods/>
+      {
+        loading ? <h1 className="blue-500">Loading</h1> : null
+      }
+      {
+        error ? <h1 className="red-900">ERROR!</h1> : null
+      }
+      <ProductItem/>
     </>
   )
 }
