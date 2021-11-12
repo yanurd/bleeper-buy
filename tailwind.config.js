779498a675ab047835fa8191e020763f20b819a7
Yancarlo Urdaneta <yancarlou@gmail.com>
2021-11-12 11:43:12 -0400

    add blippo font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   purge: [],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: false, // or 'media' or 'className'
   theme: {
     extend: {},
+    fontFamily:{
+      'blippo': ['"Blippo Black BT"', 'sans-serif']
+    }
   },
   variants: {
     extend: {},
